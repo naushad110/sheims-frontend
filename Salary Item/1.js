0b605a9c00d59aa68e4a05272c9be77831e89ff9
hamzashoukatali12 <74724218+hamzashoukatali12@users.noreply.github.com>
2022-10-24 23:22:55 +0500

    HamzaShoukat-SalaryItem-054

---
+++ Salary Item/1.js
@@ -0,0 +1,44 @@
+var vm = new Vue({
+    el: "#vue_det",
+    data: {
+      firstname: "Dr. Muhammad",
+      lastname: "Nauman",
+      htmlcontent: "<div><h1>Vue Js Template</h1></div>",
+      object: {
+        Site: " Geeks for Geeks ",
+        Framework: " Vue ",
+        Type: " Article ",
+        Subject: " v-for on a <template> ",
+      },
+      myEmp: [
+        {
+          ID: "134",
+          Name: "Ali",
+          Allownce: "2999",
+          Rent: "546",
+          Charg: "399",
+        },
+        {
+          ID: "143",
+          Name: "AHmad",
+          Allownce: "2943",
+          Rent: "543",
+          Charg: "399",
+        },
+        {
+          ID: "343",
+          Name: "Abbas",
+          Allownce: "1199",
+          Rent: "456",
+          Charg: "299",
+        },
+        {
+          ID: "234",
+          Name: "Akber",
+          Allownce: "3999",
+          Rent: "646",
+          Charg: "499",
+        },
+      ],
+    },
+  });
