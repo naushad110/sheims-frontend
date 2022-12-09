a808fcdb059b00a8c7b6c6594ddd2a4311c53780
Dr. Muhammad Nauman <mhmmd.nauman@gmail.com>
2022-12-09 22:00:46 +0500

    Update api.py

--- api.py
+++ api.py
@@ -1,18 +1,32 @@
 #https://pythonbasics.org/flask-rest-api/
+#https://www.opentechguides.com/how-to/article/python/210/flask-mysql-crud.html
+#https://flask-restful.readthedocs.io/en/latest/
+#pip install flask-restful
 from flask import Flask, render_template, jsonify
 import json 
+from flask_mysqldb import MySQL
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 app.secret_key = 'many-secret-key'
 
-@app.route('/')
-def index():
-    return json.dumps({'name': 'alice',
-                       'email': 'alice@outlook.com'})
-@app.route('/api')
-def api2():
-    return jsonify({'name': 'alice',
-                    'email': 'alice@outlook.com'})
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'assignment_db'
 
+class my_handler(Resource):
+    def get(self):
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT  * FROM teachers")
+        data = cur.fetchall()
+        return jsonify(data)
 
-app.run()
+mysql = MySQL(app)
+api = Api(app)
+
+#http://127.0.0.1:5000/teachers
+api.add_resource(my_handler, '/teachers', endpoint='get')
+
+if __name__ == "__main__":
+    app.run(debug=True)
